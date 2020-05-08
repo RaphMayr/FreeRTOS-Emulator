7c92650321ef7c6a937e89a40fb6ef167331ea69
Raphael Mayr <raphael.mayr@tum.de>
2020-05-08 09:52:04 +0200

    displaying Squre, Circle and Triangle

--- src/main.c
+++ src/main.c
@@ -42,18 +42,46 @@ void xGetButtonInput(void)
 
 #define KEYCODE(CHAR) SDL_SCANCODE_##CHAR
 
-void vDemoTask(void *pvParameters)
+void vdrawFigures(void *pvParameters)
 {
-    // structure to store time retrieved from Linux kernel
-    static struct timespec the_time;
-    static char our_time_string[100];
-    static int our_time_strings_width = 0;
-
     // Needed such that Gfx library knows which thread controlls drawing
     // Only one thread can call tumDrawUpdateScreen while and thread can call
     // the drawing functions to draw objects. This is a limitation of the SDL
     // backend.
     tumDrawBindThread();
+
+    signed short center_x = SCREEN_WIDTH/2;
+    signed short center_y = SCREEN_HEIGHT/2;
+
+    //coordinate definition for circle
+    signed short circle_x = center_x - 100;
+    signed short circle_y = center_y;
+   	signed short circle_radius = 20;
+
+    //coordinate definition for triangle 
+    coord_t upper_t;
+    coord_t lower_l_t;
+    coord_t lower_r_t;
+
+    upper_t.x = center_x;
+    upper_t.y = center_y - 20;
+
+    lower_l_t.x = center_x - 20;
+    lower_l_t.y = center_y + 20;
+
+    lower_r_t.x = center_x + 20;
+    lower_r_t.y = center_y + 20;
+
+    coord_t coord_triangle[2];
+    coord_triangle[0] = upper_t;
+    coord_triangle[1] = lower_l_t;
+    coord_triangle[2] = lower_r_t;
+
+    //coordinate definition for Square
+    signed short box_x = center_x + 100 - 20;
+    signed short box_y = center_y - 20;
+    signed short box_width = 40;
+    signed short box_height = 40;
 
     while (1) {
         tumEventFetchEvents(); // Query events backend for new events, ie. button presses
@@ -64,8 +92,8 @@ void vDemoTask(void *pvParameters)
         // resource and given back when you're finished. If the mutex is not
         // given back then no other task can access the reseource.
         if (xSemaphoreTake(buttons.lock, 0) == pdTRUE) {
-            if (buttons.buttons[KEYCODE(
-                                    Q)]) { // Equiv to SDL_SCANCODE_Q
+            if (buttons.buttons[KEYCODE(Q)]) { 
+                // Equiv to SDL_SCANCODE_Q
                 exit(EXIT_SUCCESS);
             }
             xSemaphoreGive(buttons.lock);
@@ -73,28 +101,14 @@ void vDemoTask(void *pvParameters)
 
         tumDrawClear(White); // Clear screen
 
-        clock_gettime(CLOCK_REALTIME,
-                      &the_time); // Get kernel real time
-
-        // Format our string into our char array
-        sprintf(our_time_string,
-                "There has been %ld seconds since the Epoch. Press Q to quit",
-                (long int)the_time.tv_sec);
-
-        // Get the width of the string on the screen so we can center it
-        // Returns 0 if width was successfully obtained
-        if (!tumGetTextSize((char *)our_time_string,
-                            &our_time_strings_width, NULL))
-            tumDrawText(our_time_string,
-                        SCREEN_WIDTH / 2 -
-                        our_time_strings_width / 2,
-                        SCREEN_HEIGHT / 2 - DEFAULT_FONT_SIZE / 2,
-                        TUMBlue);
+        tumDrawCircle(circle_x,circle_y,circle_radius,TUMBlue); // Draw Circle
+        tumDrawTriangle(coord_triangle, Red);
+        tumDrawFilledBox(box_x, box_y, box_width, box_height, Green);
 
         tumDrawUpdateScreen(); // Refresh the screen to draw string
 
         // Basic sleep of 1000 milliseconds
-        vTaskDelay((TickType_t)1000);
+        vTaskDelay((TickType_t)100);
     }
 }
 
@@ -125,7 +139,7 @@ int main(int argc, char *argv[])
         goto err_buttons_lock;
     }
 
-    if (xTaskCreate(vDemoTask, "DemoTask", mainGENERIC_STACK_SIZE * 2, NULL,
+    if (xTaskCreate(vdrawFigures, "drawFigures", mainGENERIC_STACK_SIZE * 2, NULL,
                     mainGENERIC_PRIORITY, &DemoTask) != pdPASS) {
         goto err_demotask;
     }
